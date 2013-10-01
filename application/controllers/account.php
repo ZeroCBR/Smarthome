91ec069531404f8fe14bfc6a8c7c102916216f93
John <john-200000000@163.com>
2013-10-01 22:04:49 +0800

    Add IPC helper which includes packing and unpacking function.

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -12,7 +12,6 @@
 		function login(){
 			if($this->account_model->login($_POST)){
 				echo "Sign in sucessfully";
-				//$this->sendmessage();
 			}
 			else
 				echo "Failed";
@@ -24,20 +23,5 @@
 			}
 		}
 
-/*		private function sendmessage(){
-			$message_queue_key = ftok('/home/john/temp/key','a');
-
-			$message_queue = msg_get_queue($message_queue_key, 0666);
-			var_dump($message_queue);
-
-			//向消息队列中写
-			msg_send($message_queue, 1, "Log in\n");
-
-			$message_queue_status = msg_stat_queue($message_queue);
-			print_r($message_queue_status);
-
-			
-		}
-*/
 	}
 ?>
