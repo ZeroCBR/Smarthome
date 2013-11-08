c7107d987556f910180dccc8f8a8d92534007b64
ZeroCBR <xraycbr@gmail.com>
2013-11-08 10:40:20 -0800

    update about

--- application/views/public/about.php
+++ application/views/public/about.php
@@ -1,3 +1,14 @@
-<?= "about page"
-
+<?php   
+        $this->load->view('share/_header'); 
 ?>
+<div class="container">
+	<div class="header">
+		
+		<?php 
+			$this->load->view('share/_topnav'); 
+		?>
+	</div>	
+</div>
+<div class="container">
+<big>Smarthome is a consumer electronics company specializing in home automation products, information, how-to videos and industry information.According to Popular Mechanics' magazine article, Insteon is not only "an effective system for connecting lighting switches and loads without extra wiring, but it also forms the basis for a more sophisticated home automation network.". Smart home is one way that implements the idea of internet of things. Through the internet, user will be able to operate their home furnishing in real time. For example, people can turn on their air conditions outside by using the internet and some receiving module in advance in such a scorcher. With the current advancements in computing power and network technology, the concept of fully automated smart homes is not far-fetched. The purpose of the Smart Home Project is to devise a set of intelligent home appliances that can provide an awareness of the users' needs, providing them with a better home life experience without overpowering them with complex technologies and intuitive user interfaces. Our main aim for the project is to improve day-to-day home life with smart computer technologies while still keeping the home life as normal as possible – we refer to this as “digitally engineering analogue home life”.</big>
+</div>
